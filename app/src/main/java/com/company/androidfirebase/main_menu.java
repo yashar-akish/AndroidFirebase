88b5627c627d931e3493c6415f57f1b0ea5e6589
yashar-akish <yashar_akish@yahoo.com>
2021-06-06 14:53:07 -0400

    uploading image in firebase

--- app/src/main/java/com/company/androidfirebase/main_menu.java
+++ app/src/main/java/com/company/androidfirebase/main_menu.java
@@ -1,23 +1,54 @@
 package com.company.androidfirebase;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 public class main_menu extends AppCompatActivity {
 
-    Button signOut;
+    Button signOut, upload;
+    ImageView profileImage;
+    ProgressBar progressBar;
+
+    Uri imageUri;
+
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+    FirebaseUser user = auth.getCurrentUser();
+    StorageReference storageReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+        storageReference = FirebaseStorage.getInstance().getReference(user.getEmail());
+
         signOut = findViewById(R.id.signOutBtn);
+        upload = findViewById(R.id.uploadBtn);
+        profileImage = findViewById(R.id.imageView);
+        progressBar = findViewById(R.id.progressBar);
+
+        progressBar.setVisibility(View.INVISIBLE);
+
         signOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,5 +58,64 @@ public class main_menu extends AppCompatActivity {
                 finish();
             }
         });
+
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadPhoto();
+            }
+        });
+
+        profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fileChooser();
+            }
+        });
+    }
+
+    public void fileChooser(){
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(i, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK){
+            imageUri = data.getData();
+            Picasso.get().load(imageUri).into(profileImage);
+        }
+    }
+
+    public void uploadPhoto(){
+        upload.setClickable(false);
+        progressBar.setVisibility(View.VISIBLE);
+
+        StorageReference reference = storageReference.child("user");
+        reference.putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        progressBar.setVisibility(View.INVISIBLE);
+                        Toast.makeText(main_menu.this, "uploaded successful", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(main_menu.this, "Sorry, There is an problem!", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                        double progress = (100 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+                        progressBar.setProgress((int)progress);
+                    }
+                });
     }
 }
