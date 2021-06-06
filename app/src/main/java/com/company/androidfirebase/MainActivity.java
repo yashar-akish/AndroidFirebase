54dd36977104d7fc250a4e15396d56de44f9a9c0
yashar-akish <yashar_akish@yahoo.com>
2021-06-05 23:19:22 -0400

    retrieving data from firebase

--- app/src/main/java/com/company/androidfirebase/MainActivity.java
+++ app/src/main/java/com/company/androidfirebase/MainActivity.java
@@ -1,22 +1,30 @@
 package com.company.androidfirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     Button send;
     EditText name;
+    TextView userName;
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference reference = database.getReference().child("users");
+    //retrieving data
+    DatabaseReference reference2 = database.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,22 @@ public class MainActivity extends AppCompatActivity {
 
         name = findViewById(R.id.editText);
         send = findViewById(R.id.button);
+        userName = findViewById(R.id.textView);
+
+        reference2.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                String realName = (String) snapshot.child("users").child("name").getValue();
+                userName.setText(realName);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         send.setOnClickListener(new View.OnClickListener() {
             @Override
