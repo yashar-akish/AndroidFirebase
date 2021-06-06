c42c6c123d7040ba770abbc7648664892a6409db
yashar-akish <yashar_akish@yahoo.com>
2021-06-05 23:01:17 -0400

    adding data to firebase

--- app/src/main/java/com/company/androidfirebase/MainActivity.java
+++ app/src/main/java/com/company/androidfirebase/MainActivity.java
@@ -3,12 +3,35 @@ package com.company.androidfirebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button send;
+    EditText name;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference reference = database.getReference().child("users");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.editText);
+        send = findViewById(R.id.button);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName =  name.getText().toString();
+                reference.child("userName").setValue(userName);
+            }
+        });
     }
 }
