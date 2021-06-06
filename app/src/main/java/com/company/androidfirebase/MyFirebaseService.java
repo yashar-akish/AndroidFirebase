4a36c9110699f53fd1c449be3e2bb94f9f26195c
yashar-akish <yashar_akish@yahoo.com>
2021-06-06 13:54:27 -0400

    notification in firebase

---
+++ app/src/main/java/com/company/androidfirebase/MyFirebaseService.java
@@ -0,0 +1,13 @@
+package com.company.androidfirebase;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
+}
