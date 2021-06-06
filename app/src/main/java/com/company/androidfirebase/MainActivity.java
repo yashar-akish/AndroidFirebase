f3732aa4dfbf87314a08d20e37fff2f7be848d6a
yashar-akish <yashar_akish@yahoo.com>
2021-06-05 23:35:40 -0400

    deleting last codes

--- app/src/main/java/com/company/androidfirebase/MainActivity.java
+++ app/src/main/java/com/company/androidfirebase/MainActivity.java
@@ -17,45 +17,9 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button send;
-    EditText name;
-    TextView userName;
-
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference reference = database.getReference().child("users");
-    //retrieving data
-    DatabaseReference reference2 = database.getReference();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        name = findViewById(R.id.editText);
-        send = findViewById(R.id.button);
-        userName = findViewById(R.id.textView);
-
-        reference2.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-
-                String realName = (String) snapshot.child("users").child("name").getValue();
-                userName.setText(realName);
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-
-        send.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String userName =  name.getText().toString();
-                reference.child("userName").setValue(userName);
-            }
-        });
     }
 }
